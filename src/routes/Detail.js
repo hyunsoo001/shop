986ef561de45c32ad51de8a802e4041a2603735d
hyunsoo <hermeto@atg.co.kr>
2024-01-25 16:48:52 +0900

    Lifecycle1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import styled from "styled-components";
 
@@ -16,7 +17,38 @@ import { useParams } from "react-router-dom";
 // `
 
 /* eslint-disable jsx-a11y/alt-text */
+
+//옛날 방법
+// class Detail2 extends React.Component {
+//   componentDidMount() {}
+
+//   componentDidUpdate() {}
+//   componentWillUnmount() {}
+// }
+
 function Detail(props) {
+  //렌더링이 다 되고나서 실행됩니다
+  // useEffect(() => {
+  //   for (var i = 0; i < 10000; i++) {
+  //     console.log(1);
+  //   }
+  //   console.log("안녕");
+  // });
+
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    // 2초 후에 알림을 숨김
+    const timeoutId = setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+
+    // 컴포넌트가 언마운트될 때 clearTimeout 호출
+    return () => clearTimeout(timeoutId);
+  }, []); // 빈 배열을 전달하여 마운트시에만 실행
+
+  let [count, setCount] = useState(0);
+
   let { id } = useParams();
 
   const result = props.shoes.find((item) => item.id === parseInt(id));
@@ -29,6 +61,22 @@ function Detail(props) {
         <YellowBtn bg="blue">버튼</YellowBtn>
         <YellowBtn bg="orange">버튼</YellowBtn>
       </Box> */}
+      <div>
+        {showAlert && (
+          <div className="alert alert-warning">2초 이내 구매 시 할인</div>
+        )}
+        {/* 나머지 컴포넌트 내용 */}
+      </div>
+
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
+
       {result ? (
         <div className="row">
           <div className="col-md-6">
