4df9def3c0818444cde723f4b8d6f598e23d690f
hyunsoo <hermeto@atg.co.kr>
2024-01-25 10:34:40 +0900

    img처리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+/* eslint-disable jsx-a11y/alt-text */
+import { Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
+import bg from "./img/bg.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Cart</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Cart</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
 
-      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + "logo192.png"} width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
